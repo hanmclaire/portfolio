664dfb99fb72876bd4ede8d515bb97cafdb2afa9
Claire Han <hanmclaire@gmail.com>
2025-10-20 00:30:32 -0400

    update next config to fix 404

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
   output: "export",           // generate static HTML in out/
   trailingSlash: true,        // /route -> /route/index.html (works with Pages)
   images: { unoptimized: true }, // no on-the-fly optimizer on GH Pages
-  // Needed when hosting at <username>.github.io/portfolio/
-  basePath: isProd ? `/${repoName}` : "",
-  assetPrefix: isProd ? `/${repoName}/` : "",
 };
 
 export default nextConfig;
